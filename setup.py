733cf6ca230f02c8a261124cdfed5ac2bafd0721
Ian Boll <mainbollian@gmail.com>
2022-07-19 22:56:16 -0400

    moved requirements into setup.py
    This is so that runtime dependencies are picked up by `pip install .`

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 from setuptools import setup
 
 if __name__ == '__main__':
-    setup()
+    setup(
+        install_requires=[
+            "megapi>=0.2.2",
+            "pyserial>=3.5"
+        ]
+    )
